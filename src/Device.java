15bfe1a1bb9324cedebfaa82ab935e8093943929
Wojtek <wojga476@gmail.com>
2018-05-28 23:22:44 +0200

    Poprawa zadania 3

---
+++ src/Device.java
@@ -0,0 +1,25 @@
+public class Device {
+
+    String code;
+    String producer;
+    String type;
+    double cena;
+
+
+    Device(String code, String producer, String type, double cena){
+        this(code, producer, type);
+//        this.producer = producer;
+//        this.type = type;
+        this.cena = cena;
+
+    }
+    Device(String code, String producer, String type){
+        this.code = code;
+        this.producer = producer;
+        this.type = type;
+
+    }
+    String getInfo() {
+        return "Kod: "+code+", producent: "+producer+", typ: "+type+", cena: "+cena;
+    }
+}
